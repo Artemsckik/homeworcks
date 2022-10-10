a86bd06214ab9c27f4536f34ee5ba05293261e61
Artemsckik <96979747+Artemsckik@users.noreply.github.com>
2022-10-10 19:14:40 +0300

    1 homework

--- lesson 1/first_homeworck.py
+++ lesson 1/first_homeworck.py
@@ -2,39 +2,56 @@
 
 a = 3
 
-a = 3
+b = 3
 
-a = 3
+c = 3
 
+print("1: ",a is b is c)
+
+print(a == b == c)
+
+print(type(a),type(b),type(c))
 # 2 переменные с одинаковыми данными (и одинаковым типом) и с разными идентификаторами
 
-b = 2
+n = [2]
 
-e = 2
+e = [2]
+
+print('2: ',n == e)
+
+print(n is e)
+
+print(type(n), type(e))
 
 # Первые 3 разны индификаторы, 2 последних индификаторы =, даные равны у всех
 
-h = 3
+h = [2]
 
-l = 3
+b = e
 
-n = 3
+c = n
 
-k = 2
+a = h
 
-k = 2
+n = a
 
-print(a)
+e = a
 
-print(b)
-
+print('3: ',a is b is c)
+print(id(a),id(b),id(c))
+print(n is e)
+print(id(n), id(e))
+print(a == b == c)
+print(type(a),type(b),type(c))
+print(n == e)
+print(type(n), type(e))
 # a1 = ...., a2 = ...., a1 == a2 True , a1 is a2 True
 
 g1 = 4
 
 g2 = 4
 
-print(g1 == g2)
+print('4: ', g1 == g2)
 
 print(g1 is g2)
 
@@ -44,6 +61,17 @@ q1 = [5]
 
 q2 = [5]
 
-print(q1 == q2)
+print('5: ', q1 == q2)
 
 print(q1 is q2)
+
+# 6
+
+z = int(3)
+
+x = float(3)
+
+
+
+print('6: ', z is x)
+print(z == x)
