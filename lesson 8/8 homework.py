e059d714d78e59f40cad0ac67562cef59a4184d4
Artemsckik <96979747+Artemsckik@users.noreply.github.com>
2022-10-30 20:32:05 +0300

    8 homework

---
+++ lesson 8/8 homework.py
@@ -0,0 +1,30 @@
+# Дан список чисел.
+# Посчитать сколько раз встречается каждое число. Использовать для подсчёта функцию.
+
+
+a = [1, 2, 2, 2, 3, 'lol']
+
+
+def count(amount=0):
+    b = a
+
+    element = input('введите элемент: ')
+
+    if not element.isdigit():
+        ...
+    else:
+
+        element = int(element)
+    while True:
+
+        if not element in b:
+            break
+
+        else:
+            b.remove(element)
+            amount = amount + 1
+
+    print(f'Ваш элемент {element}, количество элементов в списке {amount} ')
+
+
+count()
