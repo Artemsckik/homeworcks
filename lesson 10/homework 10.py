db67545f2f31a3ee4f7ec12aaa5bf35ed790728a
Artemsckik <96979747+Artemsckik@users.noreply.github.com>
2022-10-31 11:34:41 +0300

    10 homework

---
+++ lesson 10/homework 10.py
@@ -0,0 +1,20 @@
+def even():
+    while True:
+        user = input('Введите число ')
+        if not user.isdigit():
+            continue
+        else: user = [int(user)]
+        resault = filter(lambda x: x % 2 != 0, user)
+        resault = list(resault)
+        if len(resault) == 0:
+            print(f'Ваше число {user} чётное!')
+        else: print(f'Ваше число {user} не чётное!')
+
+        end = input('хотите выйти? y/yes: ')
+        if end.upper() == 'YES':
+            break
+        else: continue
+
+
+
+even()
